1dc8cf0e3879948567fe68586ef245b4c37ff93b
John Hamm <johndhamm1@gmail.com>
2019-06-26 15:45:34 -0500

    remove default app styles

--- src/containers/game.js
+++ src/containers/game.js
@@ -24,7 +24,6 @@ export default class Game extends Component {
     }
 
     handleResult(result) {
-        // console.log(result ? "right!" : 'wrong!')
         if (result) {
             this.setState({score: this.state.score + 1})
         }
